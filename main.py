73337dae5521dea2f86be6096b833dcea75ed79f
yukiel <kennylevu1@gmail.com>
2026-05-08 10:17:33 -0700

    feat: add player-asteroid collision detection

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ import pygame
 from player import Player
 from asteroid import Asteroid
 from asteroidfield import AsteroidField
+from logger import log_event
+import sys
+
 def main():
     pygame.init()
     clock = pygame.time.Clock()
@@ -31,6 +34,11 @@ def main():
                 running = False
 
         updatable.update(dt)
+        for asteroid in asteroids:
+            if player.collides_with(asteroid):
+                log_event("player_hit")
+                print("Game over!")
+                sys.exit()
 
 
         screen.fill("black")
